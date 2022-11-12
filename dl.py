65c5fdc30f55383d37970cb8e633a491e7f15ed5
harismanciripto111 <105085410+harismanciripto111@users.noreply.github.com>
2022-11-12 21:06:47 +0700

    coba

---
+++ dl.py
@@ -0,0 +1,16 @@
+import sys, os
+from skillshare import Skillshare, splash
+
+
+# or by class ID:
+# dl.download_course_by_class_id(189505397)
+
+def main():
+    dl = Skillshare("PHPSESSID=If You want this code txt Me")
+    course_url = sys.argv[1]
+    dl.download_course_by_url(course_url)
+
+
+if __name__ == "__main__":
+    splash()
+    main()
